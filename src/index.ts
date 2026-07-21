c0f0befb8ffffaa5558654902f2c6e4c3f90bd5e
Nathan-Scotty <qualitechoisie13@gmail.com>
2026-07-20 23:49:59 -0400

    update index

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import mediaRoutes from './routes/media.routes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(morgan('dev'));
